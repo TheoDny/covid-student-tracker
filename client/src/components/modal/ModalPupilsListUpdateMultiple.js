249757f9293d15183fe5141b24aaea2c93ad2c67
TheoDny <39086298+TheoDny@users.noreply.github.com>
2022-01-31 23:21:01 +0100

    feat: list update for detected case

---
+++ client/src/components/modal/ModalPupilsListUpdateMultiple.js
@@ -0,0 +1,153 @@
+import Modal from "react-bootstrap/Modal"
+import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
+import ToggleButton from "react-bootstrap/ToggleButton"
+import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup"
+import { Component } from "react"
+import "./modalDark.css"
+
+class ModalPupilsListUpdateMultiple extends Component {
+	date = new Date().toLocaleDateString("en-CA")
+	state = {
+		c_p: this.getDefaultC_p(),
+	}
+	lenghtList = 0
+	radios = [
+		{ name: "Positif", value: "positive" },
+		{ name: "Contact", value: "contact" },
+		{ name: "Rien", value: "nothing" },
+	]
+
+	getDefaultC_p() {
+		let len = this.props.pupils.length
+		let c_p = []
+		for (let i = 0; i < len; i++) {
+			let p = this.props.pupils[i]
+			if (!(p.contact || p.positive)) {
+				c_p.push({ _id: p._id, state: "contact" })
+			}
+		}
+		return c_p
+	}
+
+	handleChangeRadios(i, value) {
+		let newC_p = [...this.state.c_p]
+		newC_p[i].state = value
+		this.setState({ c_p: newC_p })
+	}
+
+	/**
+	 * Submit fucntion to update Pupils list
+	 *
+	 * @param {Event} event
+	 */
+	updatePupils(event) {
+		event.preventDefault()
+
+		let allUpdate = []
+		this.state.c_p.forEach((c_p) => {
+			if (c_p.state === "contact") {
+				allUpdate.push({
+					_id: c_p._id,
+					positive: false,
+					positiveDate: "",
+					contact: true,
+					contactDate: this.date,
+				})
+			} else if (c_p.state === "positive") {
+				allUpdate.push({
+					_id: c_p._id,
+					positive: true,
+					positiveDate: this.date,
+					contactDate: false,
+					contactDateDate: "",
+				})
+			}
+		})
+		this.props.handleUpdatePupilsList(allUpdate)
+		this.props.onHide()
+		this.setState({ c_p: this.getDefaultC_p() })
+	}
+
+	render() {
+		return (
+			<>
+				<Modal
+					show={this.props.show}
+					onHide={() => {
+						this.props.onHide()
+						this.setState(this.getDefaultC_p())
+					}}
+					size="lg"
+					centered
+					className="modalModif"
+					id="modifPupilList"
+				>
+					<Form
+						onSubmit={this.updatePupils.bind(this)}
+						className="text-white bg-dark"
+					>
+						<Modal.Header closeButton className="pt-2 pb-1">
+							<Modal.Title>Modifier la classe</Modal.Title>
+							<Form.Control
+								defaultValue={this.date}
+								onChange={(event) => (this.date = event.target.value(event))}
+								required="required"
+								type="date"
+							/>
+						</Modal.Header>
+						<Modal.Body className="pt-1 pb-2 container">
+							{this.props.pupils
+								.filter((p) => !(p.contact || p.positive))
+								.map((pupil, i) => (
+									<Form.Group
+										className="py-3 row border-bottom "
+										controlId={"pupil" + i}
+										key={i}
+									>
+										<Form.Label className="col">{`${pupil.surname} ${pupil.name}`}</Form.Label>
+										<ToggleButtonGroup
+											type="radio"
+											name={"radio" + i}
+											className="mb-2"
+											onChange={(v, e) => {
+												this.handleChangeRadios(i, v)
+											}}
+											value={this.state.c_p[i].state}
+										>
+											{this.radios.map((radio, j) => (
+												<ToggleButton
+													key={j}
+													id={`radio-${i}-${j}`}
+													type="radio"
+													variant="outline-primary"
+													value={radio.value}
+													className="mx-2"
+												>
+													{radio.name}
+												</ToggleButton>
+											))}
+										</ToggleButtonGroup>
+									</Form.Group>
+								))}
+						</Modal.Body>
+						<Modal.Footer className="py-1 d-flex justify-content-between">
+							<Button
+								variant="secondary"
+								onClick={() => {
+									this.props.onHide()
+									this.setState(this.getDefaultC_p())
+								}}
+							>
+								Close
+							</Button>
+							<Button type="submit">Sauvegarder</Button>
+						</Modal.Footer>
+					</Form>
+				</Modal>
+			</>
+		)
+	}
+}
+
+export default ModalPupilsListUpdateMultiple
--- client/src/components/tabPupil/tabPupil.js
+++ client/src/components/tabPupil/tabPupil.js
@@ -4,6 +4,7 @@ import Button from "react-bootstrap/Button"
 import "./tabPupil.css"
 import Pupil from "./pupil/Pupil"
 import ModalFormNewPupil from "../modal/ModalFormNewPupil"
+import ModalPupilListUpdateMultiple from "../modal/ModalPupilsListUpdateMultiple"
 import axios from "axios"
 
 const API_ROOT = process.env.API_ROOT || require("../../const").API_ROOT
@@ -12,6 +13,7 @@ class TabPupil extends Component {
 	todayTime = new Date().getTime()
 	state = {
 		modalFormPupilShow: false,
+		modalFormPupilsListShow: false,
 		pupils: [],
 	}
 
@@ -90,7 +92,43 @@ class TabPupil extends Component {
 				let newPupils = this.state.pupils.slice()
 				let i = newPupils.findIndex((p) => p._id === pupil._id)
 				newPupils[i] = pupil
-				this.setState({ pupils: newPupils })
+			})
+	}
+
+	/**
+	 *	replace the pupils in pupilsToUpdate in this.state.pupils with the _id
+	 *
+	 * @param {any[]} pupilsToUpdate
+	 */
+	replacePupilsInState(pupilsToUpdate) {
+		let newPupils = [...this.state.pupils]
+		let len = this.state.pupils.length
+		pupilsToUpdate.forEach((pupilUp) => {
+			let i = 0
+			while (i < len) {
+				if (this.state.pupils[i]._id === pupilUp._id) {
+					newPupils[i] = pupilUp
+				}
+			}
+		})
+
+		this.setState({ pupils: newPupils })
+	}
+
+	/**
+	 *	axios request to update a pupil in db
+	 *	then change the list "state.pupils" with setState
+	 *
+	 * @param { Array } pupils
+
+	 */
+	handleUpdatePupilsList(updatePupils) {
+		axios
+			.post(API_ROOT + "/api/pupils/updatemany", updatePupils, {
+				headers: { Authorization: `Bearer ${this.props.accessToken}` },
+			})
+			.then((res) => {
+				this.findByIdAndUpdate(res.body)
 			})
 	}
 
@@ -115,63 +153,83 @@ class TabPupil extends Component {
 
 	render() {
 		return (
-			<div id="tabPupil">
-				<div id="tab">
-					<Table className="table-dark table-bordered">
-						<thead>
-							<tr>
-								<th className="number" colSpan="1"></th>
-								<th className="surname" colSpan="1">
-									Prenom
-								</th>
-								<th className="name" colSpan="1">
-									Nom
-								</th>
-								<th className="positive" colSpan="3">
-									Positif
-								</th>
-								<th className="contact" colSpan="3">
-									Cas Contact
-								</th>
-							</tr>
-						</thead>
-						<tbody>
-							{this.state.pupils.map((p, i) => (
-								<Pupil
-									todayTime={this.todayTime}
-									key={i}
-									number={i}
-									surname={p.surname}
-									name={p.name}
-									positive={p.positive}
-									positiveDate={p.positiveDate}
-									contact={p.contact}
-									contactDate={p.contactDate}
-									deletePupil={() => this.deleteOnePupil.bind(this)(i)}
-									handleUpdatePupil={this.handleUpdatePupil.bind(this)}
-									//	unsetContact={this.unsetContact.bind(this)}
-									//	unsetPositive={this.unsetPositive.bind(this)}
-								/>
-							))}
-						</tbody>
-					</Table>
+			<>
+				<div id="tabPupil">
+					<div id="tab">
+						<Table className="table-dark table-bordered">
+							<thead>
+								<tr>
+									<th className="number" colSpan="1"></th>
+									<th className="surname" colSpan="1">
+										Prenom
+									</th>
+									<th className="name" colSpan="1">
+										Nom
+									</th>
+									<th className="positive" colSpan="3">
+										Positif
+									</th>
+									<th className="contact" colSpan="3">
+										Cas Contact
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								{this.state.pupils.map((p, i) => (
+									<Pupil
+										key={i}
+										todayTime={this.todayTime}
+										number={i + 1}
+										surname={p.surname}
+										name={p.name}
+										positive={p.positive}
+										positiveDate={p.positiveDate}
+										contact={p.contact}
+										contactDate={p.contactDate}
+										deletePupil={() => this.deleteOnePupil.bind(this)(i)}
+										handleUpdatePupil={this.handleUpdatePupil.bind(this)}
+										//	unsetContact={this.unsetContact.bind(this)}
+										//	unsetPositive={this.unsetPositive.bind(this)}
+									/>
+								))}
+							</tbody>
+						</Table>
+					</div>
+					<ModalFormNewPupil
+						show={this.state.modalFormPupilShow}
+						onHide={() => this.setState({ modalFormPupilShow: false })}
+						handleSubmitNewPupil={this.handleSubmitNewPupil.bind(this)}
+					/>
+					<div id="divButModal" className="mb-3">
+						<Button
+							className="mx-3"
+							id="modalModifList"
+							onClick={() => {
+								this.setState({ modalFormPupilsListShow: true })
+							}}
+						>
+							Cas détecté
+						</Button>
+						<Button
+							className="mx-3"
+							id="modalAddPupil"
+							onClick={() => {
+								this.setState({ modalFormPupilShow: true })
+							}}
+						>
+							Ajoutez un élève
+						</Button>
+					</div>
 				</div>
-				<ModalFormNewPupil
-					show={this.state.modalFormPupilShow}
-					onHide={() => this.setState({ modalFormPupilShow: false })}
-					handleSubmitNewPupil={this.handleSubmitNewPupil.bind(this)}
-				/>
-				<div id="divButModal">
-					<Button
-						id="modalAddPupil"
-						onClick={() => {
-							this.setState({ modalFormPupilShow: true })
-						}}
-					>
-						Ajoutez un élève
-					</Button>
-				</div>
-			</div>
+				{this.state.modalFormPupilsListShow && (
+					<ModalPupilListUpdateMultiple
+						show={this.state.modalFormPupilsListShow}
+						onHide={() => this.setState({ modalFormPupilsListShow: false })}
+						handleUpdatePupilsList={this.handleUpdatePupilsList.bind(this)}
+						pupils={this.state.pupils}
+					/>
+				)}
+			</>
 		)
 	}
 }
